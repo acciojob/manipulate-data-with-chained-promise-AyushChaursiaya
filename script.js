83f7a066fa34c674d39be81656258d6e3ab47f56
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-03-31 13:19:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+function getData() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve([1, 2, 3, 4]);
+		}, 3000)
+	});
+}
+
+function oddNumbers(arr) {
+	return arr.filter((num) => num % 2 === 0);
+}
+
+function ManipulatedNumber(arr) {
+	return arr.map((num) => num * 2)
+}
+
+
+function updateResult(text) {
+	document.getElementById('output').innerHTML = test;
+}
+
+
+async function processData() {
+
+	try{
+		const data = await getData();
+		
+			await new Promise((resolve) => { 
+			setTimeout(resolve);
+		},1000);
+		updateResult(oddNumbers(data).join(", "));
+	
+			await new Promise((resolve) => { 
+			setTimeout(resolve);
+		},2000);
+		updateResult(ManipulatedNumber(data).join(", "))
+		}catch(error) {
+		console.log(error)
+		}
+}
