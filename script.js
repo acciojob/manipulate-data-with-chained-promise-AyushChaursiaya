57cf3ca02a245e7326e6b9efefd5a432b573701e
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-03-31 15:42:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,5 @@
 // //your JS code here. If required.
-// function getData() {
-// 	return new Promise((resolve, reject) => {
-// 		setTimeout(() => {
-// 			resolve([1, 2, 3, 4]);
-// 		}, 3000)
-// 	});
-// }
 
-// function oddNumbers(arr) {
-// 	return arr.filter((num) => num % 2 === 0);
-// }
-
-// function ManipulatedNumber(arr) {
-// 	return arr.map((num) => num * 2);
-// }
-
-
-// function updateResult(text) {
-// 	document.getElementById('output').innerHTML = text;
-// }
-
-
-// async function processData() {
-
-// 	try{
-// 		const data = await getData();
-		
-// 			await new Promise((resolve) => { 
-// 			setTimeout(resolve);
-// 		},1000);
-// 		updateResult(oddNumbers(data).join(", "));
-	
-// 			await new Promise((resolve) => { 
-// 			setTimeout(resolve);
-// 		},2000);
-// 		updateResult(ManipulatedNumber(data).join(", "));
-// 		}catch(error) {
-// 		console.log(error);
-// 		}
-// }
-
-// processData();
-
-// Function to return a promise that resolves with an array after 3 seconds
 function getData() {
     return new Promise((resolve) => {
         setTimeout(() => {
